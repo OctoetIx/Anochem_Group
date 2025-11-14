c7072a046a947087e7f7220da338eafce86428e1
phakulty <anthonrichards23@gmail.com>
2025-11-14 03:13:11 +0100

    Adjust dropdown close timeout from 300ms to 200ms for improved responsiveness

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
   const handleMouseLeave = () => {
     closeTimeoutRef.current = setTimeout(() => {
       setDropdownOpen(false);
-    }, 300);
+    }, 200);
   };
 
   const toggleMenu = () => setIsOpen(!isOpen);
